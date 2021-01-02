0229cd2a61522feb509c2404777386e9933eb4b5
Vincenzo Abete <abetevincenzo@gmail.com>
2021-01-02 16:29:22 +0000

    Changed order of imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BootstrapPageComponent } from './bootstrap-page.component';
+import { FormsModule } from '@angular/forms';
 import { RandomTextPipe } from './pipes/random-text.pipe';
-import {FormsModule} from '@angular/forms';
-import {SettingsFormComponent} from './settings-form/settings-form.component';
+import { BootstrapPageComponent } from './bootstrap-page/bootstrap-page.component';
+import { SettingsFormComponent } from './settings-form/settings-form.component';
 
 @NgModule({
   declarations: [
