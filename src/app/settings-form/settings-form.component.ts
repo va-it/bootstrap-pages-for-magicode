68474ef928ef8ee190d5e8260190ba47ea41c4aa
Vincenzo Abete <v.abete@newton.ac.uk>
2021-01-04 19:23:24 +0000

    Refactored settings form component to generalise min-max functions

--- src/app/settings-form/settings-form.component.ts
+++ src/app/settings-form/settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonData } from '../common/common-data';
 import { Router } from '@angular/router';
+import {consoleTestResultHandler} from 'tslint/lib/test';
 
 @Component({
   selector: 'app-settings-form',
@@ -13,27 +14,9 @@ export class SettingsFormComponent {
   constructor(private router: Router) {
   }
 
-  public checkMaxHeader(): void {
-    if (this.commonData.maxNumberOfButtonsInHeader < this.commonData.minNumberOfButtonsInHeader) {
-      this.commonData.maxNumberOfButtonsInHeader = this.commonData.minNumberOfButtonsInHeader;
-    }
-  }
-
-  public checkMaxRows(): void {
-    if (this.commonData.maxNumberOfRows < this.commonData.minNumberOfRows) {
-      this.commonData.maxNumberOfRows = this.commonData.minNumberOfRows;
-    }
-  }
-
-  public checkMaxCards(): void {
-    if (this.commonData.maxNumberOfCardsPerRow < this.commonData.minNumberOfCardsPerRow) {
-      this.commonData.maxNumberOfCardsPerRow = this.commonData.minNumberOfCardsPerRow;
-    }
-  }
-
-  public checkMaxElements(): void {
-    if (this.commonData.maxNumberOfElementsPerCard < this.commonData.minNumberOfElementsPerCard) {
-      this.commonData.maxNumberOfElementsPerCard = this.commonData.minNumberOfElementsPerCard;
+  public ensureMaxIsNotSmallerThanMin(propertySuffix: string): void {
+    if (this.commonData['max' + propertySuffix] < this.commonData['min' + propertySuffix]) {
+      this.commonData['max' + propertySuffix] = this.commonData['min' + propertySuffix];
     }
   }
 
